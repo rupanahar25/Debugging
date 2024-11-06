0b54d621ce82d7e0d2fe6a3988afb77e2551b7d2
Rupa <rupa@Luthfuns-MacBook-Pro.local>
2024-11-06 14:48:32 +0600

    Practicing Debug

--- src/DebugPractice.java
+++ src/DebugPractice.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class DebugPractice {
+    public int calculate(int a, int b) {
+        int sum = a + b;
+        int difference = a - b;
+        return sum * difference;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        DebugPractice debugPractice = new DebugPractice();
+        int result = debugPractice.calculate(5, 3);
+        System.out.println("Result: " + result);
+    }
 }
